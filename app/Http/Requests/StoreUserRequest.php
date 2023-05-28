4389129d6ef1ae78418d414d7b3cf4953da50dc1
datodvp <gaguadato98@gmail.com>
2023-05-28 05:24:58 +0400

    refactor: user validation

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -14,9 +14,9 @@ class StoreUserRequest extends FormRequest
 	public function rules(): array
 	{
 		return [
-			'username'     => ['required', 'string', 'min:3', 'max:15', 'regex:/^[a-z]*$/'],
+			'username'     => ['required', 'string', 'min:3', 'max:15', 'regex:/^[a-z0-9]*$/'],
 			'email'        => ['required', 'string', 'max:255', 'email', 'unique:users'],
-			'password'     => ['required', 'confirmed', 'min:8', 'max:15', 'regex:/^[a-z]*$/'],
+			'password'     => ['required', 'confirmed', 'min:8', 'max:15', 'regex:/^[a-z0-9]*$/'],
 		];
 	}
 }
